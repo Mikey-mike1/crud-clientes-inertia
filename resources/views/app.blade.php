0cea395554c49a7efe5055a5654a6458da0ec2f3
Mikey-mike1 <sandovalmiguelfonseca@gmail.com>
2025-12-04 22:48:09 -0600

    Corrige blade para usar Inertia + Vite con Vue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         @routes
-        {{-- ¡CORRECCIÓN APLICADA AQUÍ! Incluye resources/css/app.css --}}
-        @vite(['resources/css/app.css', 'resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @vite('resources/js/app.js')
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
